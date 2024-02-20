5a7cda0167c81520a9f3df6d86c60e835a911394
Thihansa14 <141425007+Thihansa14@users.noreply.github.com>
2024-02-20 20:01:43 +0530

    signup signin backend
    almost completed the backend

---
+++ SignUp/config/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+
+const conn = mongoose.createConnection('mongodb+srv://T_123456:T_123456@medvault.glzwxzz.mongodb.net/?retryWrites=true&w=majority').on('open',() => {
+    console.log("Mongodb Connected");
+}).on('error',() => {
+    console.log("Mongodb error");
+})
+
+module.exports = conn;
--- SignUp/server.js
+++ SignUp/server.js
@@ -1,23 +1,24 @@
-require("dotenv").config()
-const express = require('express');
-const mongoose = require('mongoose');
+// require("dotenv").config()
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
 
-const app = express()
-app.use(express.json())
+// const app = express()
+// app.use(express.json())
 
-app.get('/', (req,res)=>{
-    res.json({mssg:'Welcome to the app'})
-})
+// app.get('/', (req,res)=>{
+//     res.json({mssg:'Welcome to the app'})
+// })
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(()=>{
-        // listen request
-        app.listen(process.env.PORT, ()=>{
-            console.log('connect to db & listing on port', process.env.PORT)
-        })
-    })
-    .catch((error)=>{
-        console.log(error)
-})
+// mongoose.connect(process.env.MONGO_URI)
+//     .then(()=>{
+//         // listen request
+//         app.listen(process.env.PORT, ()=>{
+//             console.log('connect to db & listing on port', process.env.PORT)
+//         })
+//     })
+//     .catch((error)=>{
+//         console.log(error)
+// })
 
-process.env
+// process.env
