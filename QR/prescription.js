8632e08df253ce791f9f77a4b67b7f31499d3490
Jaindu Gajanayake <125177964+Jaindu1223@users.noreply.github.com>
2024-02-21 15:56:35 +0530

    update

--- QR/prescription.js
+++ QR/prescription.js
@@ -2,10 +2,10 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const User = require('./models/User'); // Assuming you have a User model
+//const User = require('./models/User'); // Assuming you have a User model
 
 const app = express();
-const port = 3000;
+const PORT = 5000;
 const uri = "mongodb+srv://kuser:auser@medvault.glzwxzz.mongodb.net/?retryWrites=true&w=majority";
 
 app.use(bodyParser.json());
@@ -42,27 +42,27 @@ async function startServer() {
 startServer();
 
 // Define a route to get a user's email by their ID
-app.get('/users', async (req, res) => {
-  try {
-    const userId = req.params.id;
+// app.get('/users', async (req, res) => {
+//   try {
+//     const userId = req.params.id;
 
-    // Retrieve user by ID from MongoDB
-    const user = await User.findById(userId);
+//     // Retrieve user by ID from MongoDB
+//     const user = await User.findById(userId);
 
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
-    }
+//     if (!user) {
+//       return res.status(404).json({ error: 'User not found' });
+//     }
 
-    // Return the user's email
-    res.json({ email: user.email });
-  } catch (error) {
-    console.error('Error retrieving user email:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
+//     // Return the user's email
+//     res.json({ email: user.email });
+//   } catch (error) {
+//     console.error('Error retrieving user email:', error);
+//     res.status(500).json({ error: 'Internal server error' });
+//   }
+// });
 
-app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is listening at http://localhost:${PORT}`);
 });
 
 
