aa00677acd47d80de980d354d50dcd2506862321
Jaindu Gajanayake <125177964+Jaindu1223@users.noreply.github.com>
2024-02-23 15:13:35 +0530

    connecting to db

--- Qr/prescription.js
+++ Qr/prescription.js
@@ -1,11 +1,11 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 //const User = require('./models/User'); // Assuming you have a User model
 
 const app = express();
-const PORT = 2000;
+const port = '000';
 const uri = "mongodb+srv://kuser:auser@medvault.glzwxzz.mongodb.net/?retryWrites=true&w=majority";
 
 app.use(bodyParser.json());
@@ -26,8 +26,8 @@ async function startServer() {
     console.log("Connected to MongoDB");
 
     // Start the Express server
-    app.listen(PORT, () => {
-      console.log('Your server is running on port ${PORT}');
+    app.listen(port, () => {
+      console.log('Your server is running on port ${port}');
     });
 
     // Send a ping to confirm a successful connection (optional starting in v4.7)
@@ -41,27 +41,27 @@ async function startServer() {
 
 startServer();
 
-// Define a route to get a user's email by their ID
-// app.get('/users', async (req, res) => {
-//   try {
-//     const userId = req.params.id;
+//Define a route to get a user's email by their ID
+app.get('/users', async (req, res) => {
+  try {
+    const userId = req.params.id;
 
-//     // Retrieve user by ID from MongoDB
-//     const user = await User.findById(userId);
+    // Retrieve user by ID from MongoDB
+    const user = await User.findById(userId);
 
-//     if (!user) {
-//       return res.status(404).json({ error: 'User not found' });
-//     }
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
 
-//     // Return the user's email
-//     res.json({ email: user.email });
-//   } catch (error) {
-//     console.error('Error retrieving user email:', error);
-//     res.status(500).json({ error: 'Internal server error' });
-//   }
-// });
-
-app.listen(PORT, () => {
-  console.log(`Server is listening at http://localhost:${PORT}`);
+    // Return the user's email
+    res.json({ email: user.email });
+  } catch (error) {
+    console.error('Error retrieving user email:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
+
+// app.listen(port, () => {
+//   console.log(`Server is listening at http://localhost:${port}`);
+// });
 
