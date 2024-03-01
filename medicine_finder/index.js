3f8c54338c8c26d937c3bcf4acd6b52a8c01811b
AmandaK18 <kalani.20221254@iit.ac.lk>
2024-03-01 23:57:47 +0530

    nearest pharmacy

--- medicine_finder/index.js
+++ medicine_finder/index.js
@@ -1,149 +1,9 @@
 
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://kuser:auser@medvault.glzwxzz.mongodb.net/?retryWrites=true&w=majority";
-// const bodyParser = require('body-parser');
-
-// const express = require('express');
-// //const routes = require('./src/routes/testRoute')
-
-// const app = express();
-// const PORT = 4000;
-
-// app.use(bodyParser.json());
-
-// app.get('/', (req,res) =>
-//     res.send(`Node and express server is running on port ${PORT}`)
-// )
-
-// // Route to handle pharmacy search
-// app.post('/searchPharmacies', async (req, res) => {
-//   try {
-//     await client.connect();
-//     const database = client.db("stock_check");
-//     const medicinesCollection = database.collection("medicines");
-//     const pharmaciesCollection = database.collection("pharmacies");
-
-//     // Assuming your pharmacies collection has a field named "medicines"
-//     const medicineName = req.body.medicineName;
-//     const medicine = await medicinesCollection.findOne({ medicine: medicineName });
-//     // const pharmacies = await pharmaciesCollection.find({ medicines: medicineName }).toArray();
-
-//     if (!medicine) {
-//       return res.status(404).json({ error: "Medicine not found" });
-//     }
-
-  
-//   const pharmacyId = new ObjectId(medicine.pharmacy);
-//   const pharmacy = await pharmaciesCollection.findOne({ _id: pharmacyId });
-
-//   if (!pharmacy) {
-//     return res.status(404).json({ error: "Pharmacy not found" });
-//   }
-
-//   res.json({ pharmacy: pharmacy.pharmacy, city: pharmacy.city });
-// } catch (error) {
-//   console.error("Error searching pharmacies:", error);
-//   res.status(500).json({ error: "Internal server error" });
-// }
-// });
-
-
-
-// app.listen(PORT, () =>
-//     console.log(`Your serevr is running on port ${PORT}`)
-// )
-
-
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-// const express = require('express');
-// const bodyParser = require('body-parser');
-
-// const app = express();
-// const PORT = 4000;
-
-// const uri = "mongodb+srv://kuser:auser@medvault.glzwxzz.mongodb.net/?retryWrites=true&w=majority";
-
-// app.use(bodyParser.json());
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// // Connect to MongoDB and start the server
-// async function startServer() {
-//   try {
-//     await client.connect();
-//     console.log("Connected to MongoDB");
-
-//     // Start the Express server
-//     app.listen(PORT, () => {
-//       console.log(`Your server is running on port ${PORT}`);
-//     });
-
-//     // Send a ping to confirm a successful connection (optional starting in v4.7)
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } catch (error) {
-//     console.error("Error connecting to MongoDB:", error);
-//     process.exit(1); // Exit the process if there's an error
-//   }
-// }
-
-// startServer();
-
-// // Route to handle pharmacy search
-// app.post('/searchPharmacies', async (req, res) => {
-//   try {
-//     const database = client.db("stock_check");
-//     const medicinesCollection = database.collection("medicine");
-
-//     const medicineName = req.body.medicineName;
-//     const medicine = await medicinesCollection.findOne({ medicine: medicineName });
-
-//     if (!medicine) {
-//       return res.status(404).json({ error: "Medicine not found" });
-//     }
-
-//     res.json({ pharmacy: medicine.pharmacy, city: medicine.city });
-//   } catch (error) {
-//     console.error("Error searching pharmacies:", error);
-//     res.status(500).json({ error: "Internal server error" });
-//   }
-// });
-
-
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const bodyParser = require('body-parser');
+const geolib = require('geolib');
+
 
 const app = express(); 
 const PORT = 5000;
@@ -151,6 +11,9 @@ const PORT = 5000;
 const uri = "mongodb+srv://kuser:auser@medvault.glzwxzz.mongodb.net/?retryWrites=true&w=majority";
 
 app.use(bodyParser.json());
+
+const userLatitude = 80.2160345684007; // Userlatitude
+const userLongitude = 6.0317027914054435; // Userlongitude
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -169,7 +32,7 @@ async function startServer() {
 
     // Start the Express server
     app.listen(PORT, () => {
-      console.log('Your server is running ');
+      console.log(`Node and express server is running on port ${PORT}`);
     });
 
     // Send a ping to confirm a successful connection (optional starting in v4.7)
@@ -202,10 +65,23 @@ app.get('/searchPharmacies', async (req, res) => {
       return res.status(404).json({ error: "Medicine not found" });
     }
 
-    
-    const pharmacyNames = medicines.map(medicine => medicine.pharmacy);
+    const pharmaciesWithDistances = medicines.map(pharmacy => ({
+      ...pharmacy,
+      distance: geolib.getDistance(
+        { latitude: userLatitude, longitude: userLongitude },
+        { latitude: pharmacy.location.coordinates[1], longitude: pharmacy.location.coordinates[0] }
+      )
+    }));
 
-    res.json({ pharmacies: pharmacyNames });
+    pharmaciesWithDistances.sort((a, b) => a.distance - b.distance);
+
+    const nearestPharmacy = pharmaciesWithDistances[0];
+
+    res.json({ nearestPharmacy });
+
+    // const pharmacyNames = medicines.map(medicine => medicine.pharmacy);
+
+    // res.json({ pharmacies: pharmacyNames });
   } catch (error) {
     console.error("Error searching pharmacies:", error);
     res.status(500).json({ error: "Internal server error" });
