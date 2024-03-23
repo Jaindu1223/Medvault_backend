d74bb7fde50ed0b27d5c79429333613f7b45a057
Irushika01 <158060756+Irushika01@users.noreply.github.com>
2024-03-23 11:41:00 +0530

    created a patient.controller file

---
+++ controller/patient.controller.js
@@ -0,0 +1,45 @@
+
+const express = require('express');
+const router = express.Router();
+
+const userModel = require('../model/user.model');
+
+async function registerUser(req, res) {
+    try {
+        const { name,birthdate,address,email,phonenumber,NIC,password } = req.body;
+
+        const user = await userModel.findOne({ NIC});
+        if (!user) {
+            const createUser = new userModel({ name,birthdate,address,email,phonenumber,NIC,password });
+            await createUser.save();
+
+            res.json({ status: true, success: "User Created Successfully" });
+        } else {
+            res.json({ status: false, success: "User Exist" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+async function loginUser(req, res) {
+    try {
+        const { email, password } = req.body;
+
+        const user = await userModel.findOne({ email, password });
+        if (user) {
+            res.json({ status: true, success: user });
+        } else {
+            res.json({ status: false, success: "User Invalid. Please Try Again" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+module.exports = {
+    registerUser,
+    loginUser
+};
--- router/user.router.js
+++ router/user.router.js
@@ -1,44 +1,55 @@
-const router = require('express').Router();
-const userModel = require('../model/user.model');
+// const router = require('express').Router();
+// const userModel = require('../model/user.model');
 
 
-router.post('/userRegistration', async (req,res)=>{
-    try {
-        const {name,birthdate,address,email,phonenumber,NIC,password} = req.body;
+// router.post('/userRegistration', async (req,res)=>{
+//     try {
+//         const {name,birthdate,address,email,phonenumber,NIC,password} = req.body;
 
-        const user = await userModel.findOne({NIC});
-        if(!user){
-            const createuser = new userModel({name,birthdate,address,email,phonenumber,NIC,password});
-            await createuser.save();
-
-
-            res.json({status:true,success : "User Created Successfully"});
-        }else{
-            res.json({status:false,success : "User Exist"});
-        }
-
-    } catch (error) {
-        console.log(error);
-    }
+//         const user = await userModel.findOne({NIC});
+//         if(!user){
+//             const createuser = new userModel({name,birthdate,address,email,phonenumber,NIC,password});
+//             await createuser.save();
 
 
-});
+//             res.json({status:true,success : "User Created Successfully"});
+//         }else{
+//             res.json({status:false,success : "User Exist"});
+//         }
 
-router.post('/userLogin', async (req,res)=>{
-    try {
-        const {email,password} = req.body;
+//     } catch (error) {
+//         console.log(error);
+//     }
 
-        const user = await userModel.findOne({email,password});
-        if(user){
+
+// });
+
+// router.post('/userLogin', async (req,res)=>{
+//     try {
+//         const {email,password} = req.body;
+
+//         const user = await userModel.findOne({email,password});
+//         if(user){
            
-            res.json({status:true,success : user});
-        }else{
-            res.json({status:false,success : "User Invalid Please Try Again"});
-        }
+//             res.json({status:true,success : user});
+//         }else{
+//             res.json({status:false,success : "User Invalid Please Try Again"});
+//         }
 
-    } catch (error) {
-        console.log(error);
-    }
-});
+//     } catch (error) {
+//         console.log(error);
+//     }
+// });
+
+// module.exports = router;
+
+// doctorRoutes.js
+
+const express = require('express');
+const router = express.Router();
+const patientController = require('../controller/patient.controller');
+
+router.post('/userRegistration', patientController.registerUser);
+router.post('/userLogin', patientController.loginUser);
 
 module.exports = router;
