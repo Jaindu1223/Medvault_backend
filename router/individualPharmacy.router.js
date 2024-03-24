4de666af8889090fc41374b2aee8da79f76fd23f
AmandaK18 <kalani.20221254@iit.ac.lk>
2024-03-24 19:37:28 +0530

    update

--- router/individualPharmacy.router.js
+++ router/individualPharmacy.router.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const router = express.Router();
 const { watchPharmacyCollection } = require('../controller/pharmacy1.controller');
+const { watchPharmacyCollection } = require('../controller/pharmacy2.controller');
+const { watchPharmacyCollection } = require('../controller/pharmacy3.controller');
 
 watchPharmacyCollection().catch(console.error);
 
